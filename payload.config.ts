515c6e27cb102bcca8a6a74c47bb0df36278fc59
Abel Schupp <abel.schupp@gmail.com>
2024-05-03 17:54:41 +0200

    Update payload.config.ts

--- payload.config.ts
+++ payload.config.ts
@@ -90,13 +90,13 @@ export default buildConfig({
       })
     }
   },
-  // Sharp is now an optional dependency -
-  // if you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
+  // // Sharp is now an optional dependency -
+  // // if you want to resize images, crop, set focal point, etc.
+  // // make sure to install it and pass it to the config.
 
-  // This is temporary - we may make an adapter pattern
-  // for this before reaching 3.0 stable
-  sharp,
+  // // This is temporary - we may make an adapter pattern
+  // // for this before reaching 3.0 stable
+  // sharp,
   plugins: [
     cloudStorage({
       collections: {
