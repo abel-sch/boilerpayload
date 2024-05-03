2c451354c49cda5d49a56f750b84b0ff60af975a
Abel Schupp <abel.schupp@gmail.com>
2024-05-03 21:40:16 +0200

    remove logs

--- src/components/blocks/Header.tsx
+++ src/components/blocks/Header.tsx
@@ -3,7 +3,6 @@ import { Button } from "../parts/Button"
 
 export const Header = async () => {
     const links = await getMenu()
-    console.log(links)
 
     return (
         <nav className="sticky top-0 left-0 right-0 p-4 bg-gray-700 text-white flex justify-center gap-2">
