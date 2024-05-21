f7b6953ade536236eb9d36256fa925c2c4644235
Abel Schupp <abel.schupp@gmail.com>
2024-05-21 16:45:23 +0200

    change remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "*.public.blob.vercel-storage.com" }],
+    remotePatterns: [{ hostname: "*.supabase.co" }],
   },
   logging: {
     fetches: {
