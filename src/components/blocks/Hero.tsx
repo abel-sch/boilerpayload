39a1f54ba0c21a6f8fb0d40bf9807f9635551761
Abel Schupp <abel.schupp@gmail.com>
2024-05-29 15:23:26 +0200

    cleanup

--- src/components/blocks/Hero.tsx
+++ src/components/blocks/Hero.tsx
@@ -1,16 +1,17 @@
 'use client';
 
-import { DefaultHero as DefaultHeroT, Page } from "payload-types";
-import { TemplateType } from "../layouts/Page";
+import {
+    DefaultHero as DefaultHeroT,
+    HomeHero as HomeHeroT,
+    Page
+} from "payload-types";
 import { DefaultHero } from "../heroes/DefaultHero";
-
-type PrependHero<T> = T extends string ? `Hero${T}` : T;
-type HeroPropType = PrependHero<TemplateType>;
+import { HomeHero } from "../heroes/HomeHero";
 
 export default function Hero({ page }: { page: Page }) {
     if (!page.currentHero || !page[page.currentHero]) return null
     if (page.currentHero === "DefaultHero") return <DefaultHero page={page} hero={page[page.currentHero] as DefaultHeroT}/>
-    if (page.currentHero === "HomeHero") return <DefaultHero page={page} hero={page[page.currentHero] as DefaultHeroT}/>
+    if (page.currentHero === "HomeHero") return <HomeHero page={page} hero={page[page.currentHero] as HomeHeroT}/>
 
     return null;
 }
