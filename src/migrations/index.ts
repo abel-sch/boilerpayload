0dc6160f0a7f77a46d2f22b8bc3cef84618948b7
Abel Schupp <abel.schupp@gmail.com>
2024-12-15 11:52:10 +0100

    add migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241215_104141 from './20241215_104141';
+
+export const migrations = [
+  {
+    up: migration_20241215_104141.up,
+    down: migration_20241215_104141.down,
+    name: '20241215_104141'
+  },
+];
